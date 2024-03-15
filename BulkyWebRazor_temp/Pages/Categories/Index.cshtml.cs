ab6a44c90e9319746f384026ed75ba666bd097ae
rkpalt <r.p.motog3@gmail.com>
2024-03-15 12:18:38 +0000

    N-Tier Architecture - Seperate Class Libraries

--- BulkyWebRazor_temp/Pages/Categories/Index.cshtml.cs
+++ BulkyWebRazor_temp/Pages/Categories/Index.cshtml.cs
@@ -7,11 +7,12 @@ namespace BulkyWebRazor_temp.Pages.Categories
     public class IndexModel : PageModel
     {
         private readonly ApplicationDbContext _db;
-        public List<Category> CategoryList { get; set; }  
+        public List<Category> CategoryList { get; set; }
         public IndexModel(ApplicationDbContext db)
         {
             _db = db;
         }
+
         public void OnGet()
         {
             CategoryList = _db.Categories.ToList();
