63511a11bbf1f449afc72105a318525573ed347e
rkpalt <r.p.motog3@gmail.com>
2024-04-29 15:15:24 +0100

    Pushing Product to Database, Implementation of Product Repository, Configuration of Product Repository in UnitOfWork

---
+++ Bulky.DataAccess/Repository/ProductRepository.cs
@@ -0,0 +1,19 @@
+﻿using BulkyBook.DataAccess.Repository.IRepository;
+using BulkyBook.Models;
+
+namespace BulkyBook.DataAccess.Repository
+{
+    public class ProductRepository : Repository<Product>, IProductRepository
+    {
+        private readonly ApplicationDbContext _db;
+        public ProductRepository(ApplicationDbContext db) : base(db)
+        {
+            _db = db;
+        }
+
+        public void Update(Category obj)
+        {
+            _db.Categories.Update(obj);
+        }
+    }
+}
--- Bulky.DataAccess/Repository/UnitOfWork.cs
+++ Bulky.DataAccess/Repository/UnitOfWork.cs
@@ -6,10 +6,13 @@ namespace BulkyBook.DataAccess.Repository
     {
         private ApplicationDbContext _db;
         public ICategoryRepository Category { get; private set; }
+        public IProductRepository Product { get; private set; }
+
         public UnitOfWork(ApplicationDbContext db)
         {
             _db = db;
             Category = new CategoryRepository(db);
+            Product = new ProductRepository(db);
         }
         public void Save()
         {
