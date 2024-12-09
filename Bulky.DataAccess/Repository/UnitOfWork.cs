0544cb8e51925a3436c4dfd81e144f10b0370e21
rkpalt <r.p.motog3@gmail.com>
2024-12-09 08:34:59 +0000

    Added ShoppingCart and ApplicationUser to the Repository

--- Bulky.DataAccess/Repository/UnitOfWork.cs
+++ Bulky.DataAccess/Repository/UnitOfWork.cs
@@ -9,13 +9,17 @@ namespace BulkyBook.DataAccess.Repository
         public ICategoryRepository Category { get; private set; }
         public IProductRepository Product { get; private set; }
         public ICompanyRepository Company { get; private set; }
+        public IShoppingCartRepository ShoppingCart { get; private set; }
 
+        public IApplicationUserRepository ApplicationUser { get; private set; }
         public UnitOfWork(ApplicationDbContext db)
         {
             _db = db;
-            Category = new CategoryRepository(db);
-            Product = new ProductRepository(db);
-            Company = new CompanyRepository(db);
+            ShoppingCart = new ShoppingCartRepository(_db);
+            Category = new CategoryRepository(_db);
+            Product = new ProductRepository(_db);
+            Company = new CompanyRepository(_db);
+            ApplicationUser = new ApplicationUserRepository(_db);
         }
         public void Save()
         {
